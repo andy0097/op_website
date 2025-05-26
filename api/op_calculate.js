1873583a3cb68e200a8dd3f260b2cfb1444c09f6
Andrei <andrei.veres29@gmail.com>
2025-05-26 15:42:15 +0300

    allow all clients

--- api/op_calculate.js
+++ api/op_calculate.js
@@ -1,7 +1,7 @@
 
 export default function handler(req, res) {
   // Set CORS headers for every response
-  res.setHeader("Access-Control-Allow-Origin", "https://openstead.webflow.io");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
